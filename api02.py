36c4eecf2b69c22980f23bd26c8a4993ea854b3f
Leandro <leandro.a.manna@gmail.com>
2023-04-04 12:11:31 -0300

    Se modifica codigo para salir del programa

--- api02.py
+++ api02.py
@@ -1,5 +1,9 @@
 import random
 
+print("\n ------------------------------------------------ \n")
+print("\nVoy a elegir un numero aleatorio entre los numeros que tu me indiques, tambien te voy a dar la posibilidad de que elijas la cantidad de intentos que vas a tener para poder adivinar el numero. \n")
+print("\n ------------------------------------------------ \n")
+
 # Declaración de variables
 limiteInferior = int(input("Ingrese el límite inferior del rango: "))
 limiteSuperior = int(input("Ingrese el límite superior del rango: "))
@@ -34,8 +38,13 @@ while ciclosDeIntentos > 0 and not guessedRight:
 
     # Verificamos si el usuario adivinó el número secreto
     if numUsuario == numSecreto:
+        
         print("¡Felicitaciones! Adivinaste el número secreto en " + str(intentosMax - ciclosDeIntentos + 1) + " intentos.")
-        guessedRight = True
+        print("\n ------------------------------------------------ \n")
+        input("Presione ENTER para salir.")
+        exit()
+
+        """ guessedRight = True """
     else:
         # Informamos si el número ingresado es mayor o menor que el número secreto
         if numUsuario < numSecreto:
@@ -48,6 +57,9 @@ while ciclosDeIntentos > 0 and not guessedRight:
         ciclosDeIntentos -= 1
         print("Le quedan " + str(ciclosDeIntentos) + " intentos.")
 
-# Verificamos si se quedó sin intentos y le informamos el número secreto
-if ciclosDeIntentos == 0:
-    print("Se quedó sin intentos. El número secreto era: " + str(numSecreto) + ".")
+        # Verificamos si se quedó sin intentos y le informamos el número secreto
+        if ciclosDeIntentos == 0:
+            print("Se quedó sin intentos. El número secreto era: " + str(numSecreto) + ".")
+            print("\n ------------------------------------------------ \n")
+            input("Presione ENTER para salir.")
+            exit()
